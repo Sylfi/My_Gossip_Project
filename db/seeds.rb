583f6d27a2afee2e1d8c12481cc7f96eb407ef9b
Syle <sylvain.firer@gmail.com>
2019-08-15 18:03:22 +0200

    all done before migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,28 @@
 require 'faker'
 require 'pry'
 
-faker : 10.times do
+City.destroy_all
+User.destroy_all
+Gossip.destroy_all
+
+10.times do
+    City.create(name: Faker::Address.country_by_code(code: 'FR').city, zip_code: :Faker::Address.zip_code)
+end
+    puts "10 dummy cities created"
+
+10.times do
     first_name = Faker::Name.first_name
     last_name = Faker::Name.last_name
-    user = User.create!(first_name: first_name, last_name: last_name, email: "#{first_name}.#{last_name}@yopmail.com", age: :Faker.Number.between(from: 12, to: 82) )
-    #Merci Lucas t'es trop smart!
+    User.create(first_name: first_name, last_name: last_name, email: "#{first_name}.#{last_name}@yopmail.com", age: :Faker.Number.between(from: 12, to: 82), city: City.all.sample)
+    #Special thanks to Lucas ! As well as to God who created me and Lucas as we are today. 
 end
     puts "10 dummy users created"
 
-faker : 10.times do
+20 times do
+    Gossip.create(title: :Faker::Book.title, content: :Faker::ChuckNorris.fact, user: User.all.sample)
+end
+    puts "20 juicy gossips created"
+
+
+
+
