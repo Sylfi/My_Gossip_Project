47b18222c35eef809eed6241a981627b1487542d
Syle <sylvain.firer@gmail.com>
2019-08-16 09:39:02 +0200

    before static

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+#get ‘static/team', to: ’nomdufichiercontroller#nom de la méthodemetho’
+
+  resources :city
+resources :comment
+resources :gossip
+resources :user
 end
